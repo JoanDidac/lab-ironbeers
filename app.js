6f446d14ef891d210154757531cc3e597b7d2292
Joan D <joandidachr@gmail.comm>
2022-12-18 23:57:10 +0100

    first commit

--- app.js
+++ app.js
@@ -7,19 +7,30 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+app.use(express.static('public'));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // mantenemos esto? 
+
+
 
 // Register the location for handlebars partials here:
 
 // ...
 
 // Add the route handlers here:
+// app.get('/', (req, res, next) => {
+//   res.status(200).render('index');
+// })
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.get('/home', (req, res, next) => {
+  res.status(200).render('home');
+})
+
+app.get('/random', (req, res, next) => {
+  res.status(200).render('random');
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
